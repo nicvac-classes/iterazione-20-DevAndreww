9b2a4d0e6fe91d28594bdc09ea6e154c31704c49
DevAndreww <andrea.sette2@marconibari.edu.it>
2024-11-08 19:55:53 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+int main()
+{
+    int mattoncini, altezza, mattonciniNecessari;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    altezza = 0;
+
+    mattonciniNecessari = 0;
+
+    cout << "Quanti sono i mattoncini?" << endl;
+
+    cin >> mattoncini;
+
+    while(mattoncini>=mattonciniNecessari+(altezza+1))
+    {
+        altezza = altezza+1;
+
+        mattonciniNecessari = mattonciniNecessari+altezza;
+    }
+   
+    cout << "L'altezza della piramide è di " << altezza << " piani."
+   
+}
